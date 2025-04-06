5321a6066d4394f73293dee2bd7806f93230c0f9
Artur Dwornik <arturdittadwornik@gmail.com>
2025-04-06 17:35:17 +0200

    SCRUM-11: Set up basic page routing

---
+++ frontend/src/views/gradebook/Gradebook.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Gradebook.scss';
+
+function Gradebook() {
+    return (
+        <div className="gradebook">Gradebook</div>
+    )
+}
+
+export default Gradebook;
