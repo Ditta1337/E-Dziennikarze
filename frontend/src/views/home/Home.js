81670518c5f67254c8531e0b4fcf73d17098fbcd
Artur Dwornik <arturdittadwornik@gmail.com>
2025-04-10 17:50:59 +0200

    SCRUM-19: Configure web storage for user session/data

--- frontend/src/views/home/Home.js
+++ frontend/src/views/home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Home.scss';
+import {useStore} from "../../store";
+import {Button, Chip} from "@mui/material";
 
 function Home() {
+    const test = useStore((state) => state.test);
+    const increaseTest = useStore((state) => state.increaseTest);
+    const decreaseTest = useStore((state) => state.decreaseTest);
+
     return (
-        <div className="home">Home</div>
-    )
+        <div className="home">
+            <Chip label={"Current test: " + test}/>
+            <Button onClick={increaseTest} variant="contained">Add to test</Button>
+            <Button onClick={decreaseTest} variant="contained">Subtract from test</Button>
+        </div>
+    );
 }
 
 export default Home;
