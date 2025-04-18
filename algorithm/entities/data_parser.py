325e4adc64a5e9b0969429b184347646c8ca4761
Szymon Wojturski <wojtursk@student.agh.edu.pl>
2025-04-18 15:59:10 +0200

    SCRUM-29: Create function that maps algorithm and program inputs and outputs

--- algorithm/entities/data_parser.py
+++ algorithm/entities/data_parser.py
@@ -14,19 +14,18 @@ class DataParser:
     @classmethod
     def parse_input(cls, filename: str) -> tuple[list["Group"], list["Teacher"], int]:
         data = json.load(open(filename))
-        DataParser._parse_groups(data["groups"])
+        DataParser._parse_all_groups(data["groups"])
         return ([group for _, group in cls._groups_by_uuid.items()],
                 [teacher for _, teacher in cls._teachers_by_uuid.items()],
                 data["max_hours_per_day"])
 
     @staticmethod
-    def _parse_groups(groups_list) -> None:
+    def _parse_all_groups(groups_list) -> None:
         for group_data in groups_list:
-            DataParser.get_group_by_uuid(group_data["group_uuid"], group_data["subjects"])
-
+            DataParser._parse_group_by_uuid(group_data["group_uuid"], group_data["subjects"])
 
     @classmethod
-    def get_teacher_by_uuid(cls, teacher_uuid: str) -> Teacher:
+    def _parse_teacher_by_uuid(cls, teacher_uuid: str) -> Teacher:
         if not teacher_uuid in cls._teachers_by_uuid:
             teacher = Teacher(teacher_uuid)
             cls._teachers_by_uuid[teacher.uuid] = teacher
@@ -34,18 +33,18 @@ class DataParser:
         return cls._teachers_by_uuid[teacher_uuid]
 
     @classmethod
-    def get_subject_by_uuid(cls, subject_uuid: str, hours: int, teacher_uuid: str) -> Subject:
+    def _parse_subject_by_uuid(cls, subject_uuid: str, hours: int, teacher_uuid: str) -> Subject:
         if not subject_uuid in cls._subjects_by_uuid:
-            subject = Subject(subject_uuid, hours, DataParser.get_teacher_by_uuid(teacher_uuid))
+            subject = Subject(subject_uuid, hours, DataParser._parse_teacher_by_uuid(teacher_uuid))
             cls._subjects_by_uuid[subject.uuid] = subject
             cls._subjects_by_id[subject.id] = subject
         return cls._subjects_by_uuid[subject_uuid]
 
     @classmethod
-    def get_group_by_uuid(cls, group_uuid: str, subjects: list[dict]) -> Group:
+    def _parse_group_by_uuid(cls, group_uuid: str, subjects: list[dict]) -> Group:
         if not group_uuid in cls._groups_by_uuid:
             group = Group(group_uuid,
-                          [DataParser.get_subject_by_uuid(
+                          [DataParser._parse_subject_by_uuid(
                               subject["subject_uuid"], subject["hours"], subject["teacher_uuid"])
                               for subject in subjects])
             cls._groups_by_uuid[group.uuid] = group
