ea0a273091e6c8d56a52d67fb91bea01ee8400ee
Artur Dwornik <arturdittadwornik@gmail.com>
2025-05-02 21:48:20 +0200

    SCRUM-14: Add simple breadcrumbs to card header

--- frontend/src/views/not-found/NotFound.js
+++ frontend/src/views/not-found/NotFound.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './NotFound.scss'
+import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
 
 function NotFound() {
     return (
-        <div className="not-found">Page not found</div>
+        <div className="not-found">
+            <Breadcrumb/>
+        </div>
     )
 }
 
