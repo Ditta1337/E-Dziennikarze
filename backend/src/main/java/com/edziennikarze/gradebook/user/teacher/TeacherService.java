585f6318c458f489e08f47621459412d3cca68e4
MMRaspbb <maciek.malinowski.poczta@gmail.com>
2025-05-07 18:43:48 +0200

    SCRUM-23: Finished CRUD

--- backend/src/main/java/com/edziennikarze/gradebook/user/teacher/TeacherService.java
+++ backend/src/main/java/com/edziennikarze/gradebook/user/teacher/TeacherService.java
@@ -1,4 +1,45 @@
 package com.edziennikarze.gradebook.user.teacher;
 
+import com.edziennikarze.gradebook.user.User;
+import com.edziennikarze.gradebook.user.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
 public class TeacherService {
+    UserService userService;
+    TeacherRepository teacherRepository;
+
+    public Mono<User> createTeacher(Mono<User> userMono) {
+        return userService.createUser(userMono)
+                .flatMap(savedUser -> {
+                    Teacher teacher = new Teacher();
+                    teacher.setUserId(savedUser.getId());
+
+                    return teacherRepository
+                            .save(teacher)
+                            .thenReturn(savedUser);
+                });
+    }
+
+    public Mono<User> getTeacher(UUID uuid) {
+        Mono<Teacher> teacherMono = teacherRepository.findById(uuid);
+        return teacherMono.flatMap(teacher -> userService.getUser(teacher.getUserId()));
+    }
+
+    public Flux<User> getAllTeachers() {
+        Flux<Teacher> teacherFlux = teacherRepository.findAll();
+        return teacherFlux.flatMap(teacher -> userService.getUser(teacher.getUserId()));
+    }
+
+    public Mono<User> deleteTeacher(UUID uuid) {
+        return teacherRepository.findById(uuid)
+                .flatMap(teacher -> teacherRepository.deleteById(uuid)
+                        .then(userService.deleteUser(teacher.getUserId())));
+    }
 }
