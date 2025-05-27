6807918fb4acc76c9d45bc1233dc545d32827f44
MMRaspbb <maciek.malinowski.poczta@gmail.com>
2025-05-28 00:06:48 +0200

    SCRUM-37: Added user ProfileView.

--- frontend/src/components/form/fields/address-code-input/AddressCodeInput.js
+++ frontend/src/components/form/fields/address-code-input/AddressCodeInput.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { useField } from "formik";
+import {useField} from "formik";
 import * as Yup from "yup";
-import { TextField } from "@mui/material";
+import {TextField} from "@mui/material";
 import "./AddressCodeInput.scss";
 
 export const AddressCodeSchema = Yup.string()
     .matches(/^\d{2}-\d{3}$/, "Kod pocztowy musi mieć format XX-XXX")
     .required("Kod pocztowy jest wymagany");
 
-const AddressCodeInput = ({ label, ...props }) => {
+const AddressCodeInput = ({label, readOnly = false, shouldShrink = false, ...props}) => {
     const [field, meta] = useField(props);
 
     return (
         <TextField
             className="address-code-input"
             id={props.name}
+            disabled={readOnly}
             label={label}
             variant="outlined"
+            InputLabelProps={{
+                shrink: shouldShrink,
+            }}
             {...field}
             error={meta.touched && Boolean(meta.error)}
             helperText={meta.touched && meta.error}
--- frontend/src/components/form/fields/country-input/CountryInput.js
+++ frontend/src/components/form/fields/country-input/CountryInput.js
@@ -1,22 +1,26 @@
 import React from "react";
-import { useField } from "formik";
+import {useField} from "formik";
 import * as Yup from "yup";
-import { TextField } from "@mui/material";
+import {TextField} from "@mui/material";
 import "./CountryInput.scss";
 
 export const CountrySchema = Yup.string()
     .matches(/^[^\d]*$/, "Państwo nie może zawierać cyfr")
     .required("Państwo jest wymagane");
 
-const CountryInput = ({ label, ...props }) => {
+const CountryInput = ({label, readOnly = false, shouldShrink = false, ...props}) => {
     const [field, meta] = useField(props);
 
     return (
         <TextField
             className="country-input"
             id={props.name}
+            disabled={readOnly}
             label={label}
             variant="outlined"
+            InputLabelProps={{
+                shrink: shouldShrink,
+            }}
             {...field}
             error={meta.touched && Boolean(meta.error)}
             helperText={meta.touched && meta.error}
