e3d3251b62de59cb79d3aa6ef7082999123481db
Artur Dwornik <artur.dwornik@smart4aviation.aero>
2025-07-19 12:33:54 +0200

    SCRUM 70: Room CRUD and tests

---
+++ backend/src/main/java/com/edziennikarze/gradebook/lesson/planned/PlannedLessonService.java
@@ -0,0 +1,54 @@
+package com.edziennikarze.gradebook.lesson.planned;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.edziennikarze.gradebook.exception.ResourceNotFoundException;
+
+import lombok.AllArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class PlannedLessonService {
+
+    private final PlannedLessonRepository plannedLessonRepository;
+
+    public Mono<PlannedLesson> createPlannedLesson(Mono<PlannedLesson> plannedLessonMono) {
+        return plannedLessonMono.flatMap(plannedLessonRepository::save);
+    }
+
+    public Flux<PlannedLesson> getAllPlannedLessons() {
+        return plannedLessonRepository.findAll();
+    }
+
+    public Flux<PlannedLesson> getAllPlannedLessonsByGroupId(UUID groupId) {
+        return plannedLessonRepository.findAllByGroupId(groupId);
+    }
+
+    public Flux<PlannedLesson> getAllPlannedLessonsBySubjectId(UUID subjectId) {
+        return plannedLessonRepository.findAllBySubjectId(subjectId);
+    }
+
+    public Flux<PlannedLesson> getAllPlannedLessonsByTeacherId(UUID teacherId) {
+        return plannedLessonRepository.findAllByTeacherId(teacherId);
+    }
+
+    public Mono<PlannedLesson> updatePlannedLesson(Mono<PlannedLesson> plannedLessonMono) {
+        return plannedLessonMono.flatMap(plannedLesson -> plannedLessonRepository.findById(plannedLesson.getId())
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("Planned lesson with id " + plannedLesson.getId() + " not found")))
+                .flatMap(existingPlannedLesson -> {
+                    existingPlannedLesson.setSubjectId(plannedLesson.getSubjectId());
+                    existingPlannedLesson.setTeacherId(plannedLesson.getTeacherId());
+                    existingPlannedLesson.setGroupId(plannedLesson.getGroupId());
+                    existingPlannedLesson.setActive(plannedLesson.isActive());
+                    existingPlannedLesson.setStartTime(plannedLesson.getStartTime());
+                    existingPlannedLesson.setEndTime(plannedLesson.getEndTime());
+                    existingPlannedLesson.setRoomId(plannedLesson.getRoomId());
+                    existingPlannedLesson.setWeekDay(plannedLesson.getWeekDay());
+                    return plannedLessonRepository.save(existingPlannedLesson);
+                }));
+    }
+}
