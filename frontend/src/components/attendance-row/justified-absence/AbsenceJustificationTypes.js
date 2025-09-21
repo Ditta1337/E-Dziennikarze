f04abf79637f701a61bbd0d8f97e5352402b477c
MMRaspbb <maciek.malinowski.poczta@gmail.com>
2025-09-21 22:44:52 +0200

    Added attendance view, avalible under /attendance url

---
+++ frontend/src/components/attendance-row/justified-absence/AbsenceJustificationTypes.js
@@ -0,0 +1,5 @@
+export const absenceJustificationTypes = [
+    {value: "ABSENCE_MEDICAL", label: "Choroba"},
+    {value: "ABSENCE_SCHOOL_TRIP", label: "Wycieczka Szkolna"},
+    {value: "ABSENCE_OTHER", label: "Inne"}
+]
