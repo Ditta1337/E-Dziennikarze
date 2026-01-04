ff08da518a0f977902f756a90157c370eeaf2455
MMRaspbb <maciek.malinowski.poczta@gmail.com>
2026-01-04 20:31:43 +0100

    SCRUM-121 added notifications when modyfing the lesson plan

--- backend/src/main/java/com/edziennikarze/gradebook/lesson/LessonService.java
+++ backend/src/main/java/com/edziennikarze/gradebook/lesson/LessonService.java
@@ -8,6 +8,7 @@ import com.edziennikarze.gradebook.lesson.assigned.AssignedLessonRepository;
 import com.edziennikarze.gradebook.lesson.dto.DeleteRequest;
 import com.edziennikarze.gradebook.lesson.dto.Lesson;
 import com.edziennikarze.gradebook.lesson.planned.dto.PlannedLesson;
+import com.edziennikarze.gradebook.notification.NotificationService;
 import org.springframework.stereotype.Service;
 
 import com.edziennikarze.gradebook.group.studentgroup.StudentGroup;
@@ -27,6 +28,8 @@ public class LessonService {
     private final AssignedLessonRepository assignedLessonRepository;
 
     private final StudentGroupRepository studentGroupRepository;
+
+    private final NotificationService notificationService;
 
     public Flux<Lesson> getAllLessonsByStudentIdBetweenDates(UUID studentId, LocalDate from, LocalDate to) {
         return studentGroupRepository.findAllByStudentId(studentId)
@@ -50,45 +53,47 @@ public class LessonService {
                         .plannedLessonId(plannedLessonSaved.getId())
                         .date(lesson.getDate())
                         .build();
-                return assignedLessonRepository.save(assignedLesson).flatMap(assignedLessonSaved -> {
-                    lesson.setAssignedLessonId(assignedLessonSaved.getId());
-                    lesson.setPlannedLessonId(plannedLessonSaved.getId());
-                    return Mono.just(lesson);
-                });
+                return assignedLessonRepository.save(assignedLesson).flatMap(savedAssignedLesson -> sendNotificationsForCreatedLesson(lesson, savedAssignedLesson));
             });
         });
     }
 
     public Flux<Lesson> updateLessons(Flux<Lesson> lessons) {
-        return lessons.flatMap(lesson -> assignedLessonRepository.findById(lesson.getAssignedLessonId())
-                .switchIfEmpty(Mono.error(
-                        new IllegalArgumentException(
-                                "AssignedLesson not found: " + lesson.getAssignedLessonId()
-                        )
-                ))
-                .flatMap(assignedLesson -> {
+        return lessons.flatMap(lesson ->
+                assignedLessonRepository.findById(lesson.getAssignedLessonId())
+                        .switchIfEmpty(Mono.error(
+                                new IllegalArgumentException(
+                                        "AssignedLesson not found: " + lesson.getAssignedLessonId()
+                                )
+                        ))
+                        .flatMap(assignedLesson -> {
 
-                    PlannedLesson plannedLesson = makePlannedLessonFromLesson(lesson);
+                            PlannedLesson plannedLesson = makePlannedLessonFromLesson(lesson);
 
-                    return plannedLessonRepository.save(plannedLesson)
-                            .flatMap(savedPlanned -> {
-                                assignedLesson.setPlannedLessonId(savedPlanned.getId());
-                                assignedLesson.setDate(lesson.getDate());
-                                assignedLesson.setModified(true);
-                                return assignedLessonRepository.save(assignedLesson)
-                                        .map(savedAssigned -> {
-                                            lesson.setPlannedLessonId(savedPlanned.getId());
-                                            lesson.setAssignedLessonId(savedAssigned.getId());
-                                            return lesson;
-                                        });
-                            });
-                })
+                            return plannedLessonRepository.save(plannedLesson)
+                                    .flatMap(savedPlanned -> {
+                                        assignedLesson.setPlannedLessonId(savedPlanned.getId());
+                                        assignedLesson.setDate(lesson.getDate());
+                                        assignedLesson.setModified(true);
+
+                                        return assignedLessonRepository.save(assignedLesson)
+                                                .flatMap(savedAssigned -> sendNotificationsForModifiedLesson(lesson, assignedLesson, savedAssigned, savedPlanned));
+                                    });
+                        })
         );
     }
 
     public Mono<Void> deleteLessons(Mono<DeleteRequest> toDelete) {
-        return toDelete.flatMap(deleteRequest -> assignedLessonRepository.deleteAllById(deleteRequest.getIds()));
+        return toDelete.flatMapMany(deleteRequest ->
+                Flux.fromIterable(deleteRequest.getIds())
+                        .flatMap(id -> assignedLessonRepository.findLessonById(id)
+                                .flatMap(assignedLesson -> assignedLessonRepository.deleteById(id)
+                                        .then(sendNotificationsForDeletedLesson(assignedLesson))
+                                )
+                        )
+        ).then();
     }
+
 
     private PlannedLesson makePlannedLessonFromLesson(Lesson lesson) {
         return PlannedLesson.builder()
@@ -100,5 +105,89 @@ public class LessonService {
                 .groupId(lesson.getGroupId())
                 .teacherId(lesson.getTeacherId())
                 .build();
+    }
+
+    private Mono<Lesson> sendNotificationsForModifiedLesson(Lesson lesson, AssignedLesson previousAssignedLesson, AssignedLesson savedAssignedLesson, PlannedLesson savedPlannedLesson) {
+        return plannedLessonRepository.findById(lesson.getPlannedLessonId())
+                .flatMap(previousPlannedLesson -> {
+                    String message = makeModifiedLessonMessage(previousPlannedLesson, previousAssignedLesson, lesson);
+                    return studentGroupRepository.findAllByGroupId(lesson.getGroupId())
+                            .flatMap(studentGroup ->
+                                    notificationService.sendNotification(studentGroup.getStudentId(), message)
+                            )
+                            .then(notificationService.sendNotification(lesson.getTeacherId(), message).thenReturn(lesson)
+                            )
+                            .map(l -> {
+                                l.setPlannedLessonId(savedPlannedLesson.getId());
+                                l.setAssignedLessonId(savedAssignedLesson.getId());
+                                return l;
+                            });
+                });
+    }
+
+    private Mono<Lesson> sendNotificationsForCreatedLesson(Lesson lesson, AssignedLesson savedAssignedLesson) {
+        String message = makeCreatedLessonMessage(lesson);
+        return studentGroupRepository.findAllByGroupId(lesson.getGroupId())
+                .flatMap(studentGroup ->
+                        notificationService.sendNotification(studentGroup.getStudentId(), message)
+                )
+                .then(notificationService.sendNotification(lesson.getTeacherId(), message).thenReturn(lesson))
+                .map(l -> {
+                    l.setPlannedLessonId(savedAssignedLesson.getPlannedLessonId());
+                    l.setAssignedLessonId(savedAssignedLesson.getId());
+                    return l;
+                });
+    }
+
+    private Mono<Void> sendNotificationsForDeletedLesson(Lesson lesson) {
+        String message = makeDeletedLessonMessage(lesson);
+        return studentGroupRepository.findAllByGroupId(lesson.getGroupId())
+                .flatMap(studentGroup ->
+                        notificationService.sendNotification(studentGroup.getStudentId(), message)
+                )
+                .then(notificationService.sendNotification(lesson.getTeacherId(), message).then());
+    }
+
+    private String makeModifiedLessonMessage(PlannedLesson previousPlannedLesson, AssignedLesson previousAssignedLesson, Lesson modifiedLesson) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Zmodyfikowano lekcję " + previousPlannedLesson.getStartTime() + " - " + previousPlannedLesson.getEndTime() + " w dniu " + previousAssignedLesson.getDate() + " : ");
+        if (!previousAssignedLesson.getDate().equals(modifiedLesson.getDate())) {
+            stringBuilder.append("Nowa lekcja w dniu: " + modifiedLesson.getDate() + ", ");
+        }
+        if (!previousPlannedLesson.getStartTime().equals(modifiedLesson.getStartTime()) || !previousPlannedLesson.getEndTime().equals(modifiedLesson.getEndTime())) {
+            stringBuilder.append("Nowa lekcja w godzinach: " + modifiedLesson.getStartTime() + " - " + modifiedLesson.getEndTime() + ", ");
+        }
+        if (!previousPlannedLesson.getSubjectId().equals(UUID.fromString(modifiedLesson.getSubjectId()))) {
+            stringBuilder.append("Nowa lekcja o temacie: " + modifiedLesson.getSubject() + ", ");
+        }
+        if (!previousPlannedLesson.getRoomId().equals(modifiedLesson.getRoomId())) {
+            stringBuilder.append("Nowa lekcja w pokoju: " + modifiedLesson.getRoom() + ", ");
+        }
+        if (!previousPlannedLesson.getGroupId().equals(modifiedLesson.getGroupId())) {
+            stringBuilder.append("Nowa lekcja z grupą: " + modifiedLesson.getGroupCode());
+        }
+        return stringBuilder.toString();
+    }
+
+    private String makeCreatedLessonMessage(Lesson lesson) {
+        return "Stworzono lekcję " + makeLessonDescription(lesson);
+    }
+
+    private String makeDeletedLessonMessage(Lesson lesson) {
+        return "Odwołano lekcję " + makeLessonDescription(lesson);
+    }
+
+    private String makeLessonDescription(Lesson lesson) {
+        return "w godzinach: " +
+                lesson.getStartTime() +
+                " - " +
+                lesson.getEndTime() + " w dniu: " +
+                lesson.getDate() +
+                ", o temacie: " +
+                lesson.getSubject() +
+                ", w pokoju: " +
+                lesson.getRoom() +
+                ", z grupą: " +
+                lesson.getGroupCode();
     }
 }
--- backend/src/main/java/com/edziennikarze/gradebook/lesson/assigned/AssignedLessonRepository.java
+++ backend/src/main/java/com/edziennikarze/gradebook/lesson/assigned/AssignedLessonRepository.java
@@ -2,13 +2,44 @@ package com.edziennikarze.gradebook.lesson.assigned;
 
 import java.util.UUID;
 
+import com.edziennikarze.gradebook.lesson.dto.Lesson;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface AssignedLessonRepository extends ReactiveCrudRepository<AssignedLesson, UUID> {
 
     Flux<AssignedLesson> findAllByCancelled(boolean cancelled);
+
+    @Query("""
+                SELECT
+                    a.id AS assigned_lesson_id,
+                    p.id AS planned_lesson_id,
+                    p.group_id AS group_id,
+                    g.group_code AS group_code,
+                    a.date AS date,
+                    a.cancelled AS cancelled,
+                    a.modified AS modified,
+                    s.id AS subject_id,
+                    s.name AS subject,
+                    p.start_time AS start_time,
+                    p.end_time AS end_time,
+                    p.week_day AS week_day,
+                    r.id AS room_id,
+                    r.room_code AS room,
+                    p.teacher_id AS teacher_id
+                FROM planned_lessons p
+                INNER JOIN assigned_lessons a ON a.planned_lesson_id = p.id
+                INNER JOIN subjects s ON s.id = p.subject_id
+                INNER JOIN rooms r ON r.id = p.room_id
+                INNER JOIN groups g ON g.id = p.group_id
+                WHERE a.id = :assignedLessonId
+            """)
+    Mono<Lesson> findLessonById(UUID assignedLessonId);
+
 }
